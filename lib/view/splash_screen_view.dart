c2425694b9defe54ea2d6e5d59244f23036195f0
Ritesh230087 <riteshdaas01@gmail.com>
2025-05-19 09:40:45 +0545

    Splash Screen

---
+++ lib/view/splash_screen_view.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+
+            // Logo
+            Image.asset(
+              'assets/logo/rolo_logo.jpeg', 
+              width: 200,
+            ),
+
+            const SizedBox(height: 30),
+
+            // App name
+            const Text(
+              'ROLO',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            // Tagline
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Text(
+                'Crafted Elegance from Nepal – Welcome to ROLO',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+
+            const Spacer(),
+
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
