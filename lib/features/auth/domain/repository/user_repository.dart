47acf1ecac3455b9c9d8ded7d6412c223d2dae58
Ritesh230087 <riteshdaas01@gmail.com>
2025-06-18 12:29:36 +0545

    Repository

--- lib/features/auth/domain/repository/user_repository.dart
+++ lib/features/auth/domain/repository/user_repository.dart
@@ -1,1 +1,13 @@
-abstract
+import 'package:dartz/dartz.dart';
+import 'package:rolo/core/error/failure.dart';
+import 'package:rolo/features/auth/domain/entity/user_entity.dart';
+
+abstract interface class IUserRepository{
+   Future<Either<Failure, void>> registerUser(UserEntity user);
+
+  Future<Either<Failure, String>> loginUser(
+    String email,
+    String password,
+  );
+
+}
