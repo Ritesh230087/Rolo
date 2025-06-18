cb1d9d3f1fe3c1486567c4129c3a8c1ce51e9b93
Ritesh230087 <riteshdaas01@gmail.com>
2025-06-18 21:43:59 +0545

    Service Locator

--- lib/app/service_locator/service_locator.dart
+++ lib/app/service_locator/service_locator.dart
@@ -1,1 +1,59 @@
+import 'package:get_it/get_it.dart';
+import 'package:rolo/core/network/hive_service.dart';
+import 'package:rolo/features/auth/data/data_source/local_datasource/user_local_data_source.dart';
+import 'package:rolo/features/auth/data/repository/local_repository/user_local_repository.dart';
+import 'package:rolo/features/auth/domain/use_case/user_login_usecase.dart';
+import 'package:rolo/features/auth/domain/use_case/user_register_usecase.dart';
+import 'package:rolo/features/auth/presentation/view_model/login_view_model/login_view_model.dart';
+import 'package:rolo/features/auth/presentation/view_model/register_view_model/register_view_model.dart';
+import 'package:rolo/features/splash/presentation/view_model/splash_view_model.dart';
+
 final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  await _initHiveService();
+  await _initAuthModule();
+  await _initSplashModule();
+}
+
+Future<void> _initHiveService() async {
+  serviceLocator.registerLazySingleton(() => HiveService());
+}
+
+Future<void> _initAuthModule() async {
+  serviceLocator.registerFactory(
+    () => UserLocalDataSource(hiveService: serviceLocator<HiveService>()),
+  );
+
+  serviceLocator.registerFactory(
+    () => UserLocalRepository(
+      userLocalDataSource: serviceLocator<UserLocalDataSource>(),
+    ),
+  );
+
+  serviceLocator.registerFactory(
+    () => UserLoginUsecase(
+      userRepository: serviceLocator<UserLocalRepository>(),
+    ),
+  );
+
+  serviceLocator.registerFactory(
+    () => UserRegisterUsecase(
+      userRepository: serviceLocator<UserLocalRepository>(),
+    ),
+  );
+
+  serviceLocator.registerFactory(
+    () => RegisterViewModel(
+      serviceLocator<UserRegisterUsecase>(),
+    ),
+  );
+  
+  serviceLocator.registerFactory(
+    () => LoginViewModel(serviceLocator<UserLoginUsecase>()),
+  );
+}
+
+Future<void> _initSplashModule() async {
+  serviceLocator.registerFactory(() => SplashViewModel());
+}
