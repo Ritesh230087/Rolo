caa918afb31b20a547374a8b7826c41c46cd33a8
Ritesh230087 <riteshdaas01@gmail.com>
2025-06-18 20:25:02 +0545

    Login Usecase

---
+++ lib/features/auth/domain/use_case/user_login_usecase.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:rolo/app/use_case/usecase.dart';
+import 'package:rolo/core/error/failure.dart';
+import 'package:rolo/features/auth/domain/repository/user_repository.dart';
+
+class LoginParams extends Equatable {
+  final String email;
+  final String password;
+
+  const LoginParams({required this.email, required this.password});
+
+  const LoginParams.initial() : email = '', password = '';
+  @override
+  List<Object?> get props => [email, password];
+}
+
+class UserLoginUsecase implements UsecaseWithParams<String, LoginParams> {
+  final IUserRepository _userRepository;
+
+  UserLoginUsecase({required IUserRepository userRepository})
+    : _userRepository = userRepository;
+
+  @override
+  Future<Either<Failure, String>> call(LoginParams params) async {
+    return await _userRepository.loginUser(
+      params.email,
+      params.password,
+    );
+  }
+}
