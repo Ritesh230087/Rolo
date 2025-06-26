7ac5c7724429087ff587ef73f43d4489a720fce2
Ritesh230087 <riteshdaas01@gmail.com>
2025-06-26 16:52:55 +0545

    User Api Model

---
+++ lib/features/auth/data/model/user_api_model.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_api_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserApiModel _$UserApiModelFromJson(Map<String, dynamic> json) => UserApiModel(
+      userId: json['_id'] as String?,
+      firstName: json['firstName'] as String,
+      lastName: json['lastName'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+    );
+
+Map<String, dynamic> _$UserApiModelToJson(UserApiModel instance) =>
+    <String, dynamic>{
+      '_id': instance.userId,
+      'firstName': instance.firstName,
+      'lastName': instance.lastName,
+      'email': instance.email,
+      'password': instance.password,
+    };
