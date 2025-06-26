30f3f10620478191b3f3d0207c0f8bb46f72dcfe
Ritesh230087 <riteshdaas01@gmail.com>
2025-06-26 18:50:42 +0545

    Service Locator

--- lib/app/service_locator/service_locator.dart
+++ lib/app/service_locator/service_locator.dart
@@ -1,7 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:rolo/core/network/api_service.dart';
 import 'package:rolo/core/network/hive_service.dart';
 import 'package:rolo/features/auth/data/data_source/local_datasource/user_local_data_source.dart';
+import 'package:rolo/features/auth/data/data_source/remote_data_source/user_remote_data_source.dart';
 import 'package:rolo/features/auth/data/repository/local_repository/user_local_repository.dart';
+import 'package:rolo/features/auth/data/repository/remote_repository/user_remote_repository.dart';
 import 'package:rolo/features/auth/domain/use_case/user_login_usecase.dart';
 import 'package:rolo/features/auth/domain/use_case/user_register_usecase.dart';
 import 'package:rolo/features/auth/presentation/view_model/login_view_model/login_view_model.dart';
@@ -13,7 +17,12 @@ final serviceLocator = GetIt.instance;
 Future<void> initDependencies() async {
   await _initHiveService();
   await _initAuthModule();
+  await _initApiService();
   await _initSplashModule();
+}
+
+Future<void> _initApiService() async {
+  serviceLocator.registerLazySingleton(() => ApiService(Dio()));
 }
 
 Future<void> _initHiveService() async {
@@ -26,20 +35,31 @@ Future<void> _initAuthModule() async {
   );
 
   serviceLocator.registerFactory(
+    () => UserRemoteDataSource(apiService: serviceLocator<ApiService>()),
+  );
+
+
+  serviceLocator.registerFactory(
     () => UserLocalRepository(
       userLocalDataSource: serviceLocator<UserLocalDataSource>(),
     ),
   );
 
   serviceLocator.registerFactory(
+    () => UserRemoteRepository(
+      userRemoteDataSource: serviceLocator<UserRemoteDataSource>(),
+    ),
+  );
+
+  serviceLocator.registerFactory(
     () => UserLoginUsecase(
-      userRepository: serviceLocator<UserLocalRepository>(),
+      userRepository: serviceLocator<UserRemoteRepository>(),
     ),
   );
 
   serviceLocator.registerFactory(
     () => UserRegisterUsecase(
-      userRepository: serviceLocator<UserLocalRepository>(),
+      userRepository: serviceLocator<UserRemoteRepository>(),
     ),
   );
 
