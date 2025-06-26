1691cae215ec08226ed99b1b4e1ac320c7336ed9
Ritesh230087 <riteshdaas01@gmail.com>
2025-06-26 17:59:28 +0545

    User Remote Data Source

---
+++ lib/features/auth/data/data_source/remote_data_source/user_remote_data_source.dart
@@ -0,0 +1,55 @@
+import 'package:dio/dio.dart';
+import 'package:rolo/app/constant/api_endpoints.dart';
+import 'package:rolo/core/network/api_service.dart';
+import 'package:rolo/features/auth/data/data_source/user_data_source.dart';
+import 'package:rolo/features/auth/data/model/user_api_model.dart';
+import 'package:rolo/features/auth/domain/entity/user_entity.dart';
+
+class UserRemoteDataSource implements IUserDataSource{
+  final ApiService _apiService;
+  UserRemoteDataSource({required ApiService apiService})
+    : _apiService = apiService;
+
+
+  @override
+  Future<String> loginUser(String email, String password) async{
+      try {
+      final response = await _apiService.dio.post(
+        ApiEndpoints.login,
+        data: {'email': email, 'password': password},
+      );
+      if (response.statusCode == 200) {
+        final str = response.data['token'];
+        return str;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } on DioException catch (e) {
+      throw Exception('Failed to login user: ${e.message}');
+    } catch (e) {
+      throw Exception('Failed to login user: $e');
+    }
+  }
+
+  @override
+  Future<void> registerUser(UserEntity user) async{
+    try {
+      final userApiModel = UserApiModel.fromEntity(user);
+      final response = await _apiService.dio.post(
+        ApiEndpoints.register,
+        data: userApiModel.toJson(),
+      );
+      if (response.statusCode == 200) {
+        return;
+      } else {
+        throw Exception(
+          'Failed to register user: ${response.statusMessage}',
+        );
+      }
+    } on DioException catch (e) {
+      throw Exception('Failed to register user: ${e.message}');
+    } catch (e) {
+      throw Exception('Failed to register user: $e');
+    }
+  }
+}
