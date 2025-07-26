7fabff85d8e10379577a3da4f59719c191759f9c
Ritesh230087 <riteshdaas01@gmail.com>
2025-07-26 19:13:34 +0545

    user repository

--- lib/features/auth/domain/repository/user_repository.dart
+++ lib/features/auth/domain/repository/user_repository.dart
@@ -2,12 +2,16 @@ import 'package:dartz/dartz.dart';
 import 'package:rolo/core/error/failure.dart';
 import 'package:rolo/features/auth/domain/entity/user_entity.dart';
 
-abstract interface class IUserRepository{
-   Future<Either<Failure, void>> registerUser(UserEntity user);
+abstract interface class IUserRepository {
+  Future<Either<Failure, void>> registerUser(UserEntity user);
 
   Future<Either<Failure, String>> loginUser(
     String email,
     String password,
   );
 
+  // --- ADD THIS LINE ---
+  // This is the contract for the repository layer.
+  Future<Either<Failure, void>> registerFCMToken(String token);
+  // -----------------------
 }
